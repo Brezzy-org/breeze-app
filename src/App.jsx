0c632ed26c3543adf40ccb1add04a39ddf172991
Jacinta Akpene Sadzi <jakpesad20@gmail.com>
2024-11-05 15:15:09 +0000

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,85 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
+import Home from "./pages/home";
+import About from "./pages/about";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from './components/Hero';
+import Login from './pages/forms/Login';
+import Register from './pages/forms/Register';
+import DashboardLayout from './Layouts/DashbordLayout';
+import DashboardHome from './pages/dashboard/DashboardHome';
+import Mood from './pages/dashboard/Mood';
+import Breathing from './pages/dashboard/Breathing';
+import Reminders from './pages/dashboard/Reminders';
+import SupportChat from './pages/dashboard/SupportChat';
+import Logout from './pages/dashboard/Logout';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+   {
+    path: "/navbar",
+    element:<Navbar/>
+   },
+   {
+    path: "/footer",
+    element:<Footer/>
+   },
+   {
+    path: "/hero",
+    element:<Hero/>
+   },
+   {
+    path: "/register",
+    element:<Register/>
+   },
+   {
+    path: "/login",
+    element:<Login/>
+   },
+    
+   {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true, 
+        element: <DashboardHome />,
+      },
+      {
+        path: "mood", 
+        element: <Mood />,
+      },
+      {
+        path: "breathing",
+        element: <Breathing />,
+      },
+      {
+        path: "reminders",
+        element: <Reminders />,
+      },
+      {
+        path: "supportchat",
+        element: <SupportChat />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+      },
+    ],
+  }
   
 
-  return (
-    <div> <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
-  )
-}
+  ]);
 
+  return <RouterProvider router={router} />;
+}
 export default App
